d4e6e39838d7735e2bdc5b8c04f05095bf60d6b0
stefan_pavlovic <spstefan5@gmail.com>
2021-02-02 00:06:57 +0100

    Day 1 implemented

---
+++ AdventOfCode2020/DayOne.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    public static class DayOne
+    {
+        public static Tuple<int, int> FindSumPair(IEnumerable<int> input, int sum)
+        {
+            var orderedInput = input.OrderBy(i => i);
+            var firstHalf = orderedInput.TakeWhile(i => i < sum / 2).ToList();
+            var secondHalf = orderedInput.SkipWhile(i => i >= sum / 2 && i < sum).ToList();
+
+            var result = firstHalf.Where(i => secondHalf.Contains(sum - i)).ToList();
+
+            if (!result.Any()) return null;
+
+            return new Tuple<int, int>(result.Single(), sum - result.Single());
+
+        }
+
+        public static Tuple<int, int, int> FindSumTriplet(IEnumerable<int> input, int sum)
+        {
+            var orderedInput = input.OrderByDescending(i => i).ToList();
+
+            foreach (var item in orderedInput)
+            {
+                var res = FindSumPair(orderedInput, sum - item);
+
+                if (res?.Item2 != null && orderedInput.Contains(res.Item1) && orderedInput.Contains(res.Item2))
+                {
+                    return new Tuple<int, int, int>(item, res.Item1, res.Item2);
+                }
+            }
+
+            throw new ArgumentException();
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode2020;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var delimiters = new char[] { '\r', '\n'};
+            
+            var input = Resources.DayOne.Split(delimiters)
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => int.Parse(s.Trim()));
+
+            Console.WriteLine(input.Count());
+
+            var result = DayOne.FindSumPair(input, 2020);
+            var multipliedResult = result.Item1 * result.Item2;
+
+            var resultTriplet = DayOne.FindSumTriplet(input, 2020);
+            var multipledTripletResult = resultTriplet.Item1 * resultTriplet.Item2 * resultTriplet.Item3;
+        }
+
+
+    }
+}
